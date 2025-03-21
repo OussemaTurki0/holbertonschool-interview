e28da594955305617f93e846190c276e89c2ce2e
oussematurki0 <oussematurki0@gmail.com>
2025-03-21 02:10:00 +0100

    commit

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -3,46 +3,46 @@
 #include "menger.h"
 
 /**
- * should_be_empty - Checks if the current cell should be empty
- * @r: The current row of the cell
- * @c: The current column of the cell
+ * is_empty - Determines if the current cell should be empty
+ * @row: The current row of the cell
+ * @col: The current column of the cell
  *
  * Return: 1 if the cell should be empty, 0 otherwise
  */
-int should_be_empty(int r, int c)
+int is_empty(int row, int col)
 {
-    while (r > 0 && c > 0)
-    {
-        if (r % 3 == 1 && c % 3 == 1)
-            return (1);
-        r /= 3;
-        c /= 3;
-    }
-    return (0);
+	while (row > 0 && col > 0)
+	{
+		if (row % 3 == 1 && col % 3 == 1)
+			return (1);
+		row /= 3;
+		col /= 3;
+	}
+	return (0);
 }
 
 /**
- * draw_menger_sponge - Renders a 2D Menger Sponge of a given level
- * @n: The level of the Menger Sponge to be drawn
+ * menger - Draws a 2D Menger Sponge of a given level
+ * @level: The level of the Menger Sponge to draw
  */
-void draw_menger_sponge(int n)
+void menger(int level)
 {
-    int size, i, j;
+	int size, row, col;
 
-    if (n < 0)
-        return;
+	if (level < 0)
+		return;
 
-    size = pow(3, n);
+	size = pow(3, level);
 
-    for (i = 0; i < size; i++)
-    {
-        for (j = 0; j < size; j++)
-        {
-            if (should_be_empty(i, j))
-                putchar(' ');
-            else
-                putchar('#');
-        }
-        putchar('\n');
-    }
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			if (is_empty(row, col))
+				putchar(' ');
+			else
+				putchar('#');
+		}
+		putchar('\n');
+	}
 }
