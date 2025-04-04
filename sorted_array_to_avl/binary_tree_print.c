2ef899b12c587e90834bf9f8c666aaddcca7b939
oussematurki0 <oussematurki0@gmail.com>
2025-04-04 14:56:20 +0200

    commit

--- sorted_array_to_avl/binary_tree_print.c
+++ sorted_array_to_avl/binary_tree_print.c
@@ -1,34 +1,99 @@
-#include "binary_trees.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "binary_trees.h"
+
+/* Original code from http://stackoverflow.com/a/13755911/5184480 */
 
 /**
- * print_tree - Prints a binary tree in 2D
+ * print_t - Stores recursively each level in an array of strings
  *
- * @tree: Pointer to the root node
- * @level: Current depth
- * @spaces: Whitespace for formatting
+ * @tree: Pointer to the node to print
+ * @offset: Offset to print
+ * @depth: Depth of the node
+ * @s: Buffer
+ *
+ * Return: length of printed tree after process
  */
-static void print_tree(const binary_tree_t *tree, int level, int spaces)
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s)
 {
-    if (!tree)
-        return;
+	char b[6];
+	int width, left, right, is_left, i;
 
-    print_tree(tree->right, level + 1, spaces);
-
-    for (int i = 0; i < level * spaces; i++)
-        printf(" ");
-    printf("(%03d)\n", tree->n);
-
-    print_tree(tree->left, level + 1, spaces);
+	if (!tree)
+		return (0);
+	is_left = (tree->parent && tree->parent->left == tree);
+	width = sprintf(b, "(%03d)", tree->n);
+	left = print_t(tree->left, offset, depth + 1, s);
+	right = print_t(tree->right, offset + left + width, depth + 1, s);
+	for (i = 0; i < width; i++)
+		s[depth][offset + left + i] = b[i];
+	if (depth && is_left)
+	{
+		for (i = 0; i < width + right; i++)
+			s[depth - 1][offset + left + width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	else if (depth && !is_left)
+	{
+		for (i = 0; i < left + width; i++)
+			s[depth - 1][offset - width / 2 + i] = '-';
+		s[depth - 1][offset + left + width / 2] = '.';
+	}
+	return (left + width + right);
 }
 
 /**
- * binary_tree_print - Public function to print the binary tree
- * @tree: Pointer to the root node
+ * _height - Measures the height of a binary tree
+ *
+ * @tree: Pointer to the node to measures the height
+ *
+ * Return: The height of the tree starting at @node
+ */
+static size_t _height(const binary_tree_t *tree)
+{
+	size_t height_l;
+	size_t height_r;
+
+	height_l = tree->left ? 1 + _height(tree->left) : 0;
+	height_r = tree->right ? 1 + _height(tree->right) : 0;
+	return (height_l > height_r ? height_l : height_r);
+}
+
+/**
+ * binary_tree_print - Prints a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to print
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    print_tree(tree, 0, 6);
+	char **s;
+	size_t height, i, j;
+
+	if (!tree)
+		return;
+	height = _height(tree);
+	s = malloc(sizeof(*s) * (height + 1));
+	if (!s)
+		return;
+	for (i = 0; i < height + 1; i++)
+	{
+		s[i] = malloc(sizeof(**s) * 255);
+		if (!s[i])
+			return;
+		memset(s[i], 32, 255);
+	}
+	print_t(tree, 0, 0, s);
+	for (i = 0; i < height + 1; i++)
+	{
+		for (j = 254; j > 1; --j)
+		{
+			if (s[i][j] != ' ')
+				break;
+			s[i][j] = '\0';
+		}
+		printf("%s\n", s[i]);
+		free(s[i]);
+	}
+	free(s);
 }
--- sorted_array_to_avl/binary_trees.h
+++ sorted_array_to_avl/binary_trees.h
@@ -1,8 +1,7 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
-#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -12,18 +11,18 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+/* AVL Tree type */
 typedef struct binary_tree_s avl_t;
 
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 avl_t *sorted_array_to_avl(int *array, size_t size);
+void binary_tree_print(const binary_tree_t *);
 
-#endif /* BINARY_TREES_H */
+#endif
