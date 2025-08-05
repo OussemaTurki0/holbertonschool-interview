15b6003550e7f12f168835eb95b8e069b9e8d0f1
oussematurki0 <oussematurki0@gmail.com>
2025-08-05 15:27:00 +0200

    commit

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+def pascal_triangle(n):
+    """
+    Returns a list of lists representing Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]  # First row is always [1]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]  # First element of the row is always 1
+
+        # Build the new row by adding pairs of adjacent elements from the previous row
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)  # Last element is always 1
+        triangle.append(new_row)
+
+    return triangle
---
+++ pascal_triangle/Readme.md
@@ -0,0 +1,5 @@
+# Pascal's Triangle
+
+## Task
+
+Write a function `pascal_triangle(n)` that returns a list of lists of integers representing the Pascal’s triangle of `n`.
